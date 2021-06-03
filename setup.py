7437769a0309ba832ebb6168b23af3a57be5e7e0
Alexander Ryzhov <72242438+ryzhovalex@users.noreply.github.com>
2021-06-03 13:01:14 +0400

    Patch setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 __author__ = "Alexander Ryzhov"
 __email__ = "thed4rkof@gmail.com"
-__version__ = "0.1.0-proto.1"
+__version__ = "0.1.1-proto.1"
 
 # here setup, manage and run operations will be defined
